cc9e670f44198130efc480ee86c0e363b2f06c19
iksadNorth <iksadNorth@gmail.com>
2023-10-05 00:48:39 +0900

    기본적인 WebSocket을 이용한 채팅 서버 구현

--- src/main/java/com/iksad/chattinglab/ChattingLabApplication.java
+++ src/main/java/com/iksad/chattinglab/ChattingLabApplication.java
@@ -3,6 +3,7 @@ package com.iksad.chattinglab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//https://yjkim-dev.tistory.com/65
 @SpringBootApplication
 public class ChattingLabApplication {
 
