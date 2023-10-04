ccae0d7e6db116a0df112030069537a6617f364a
iksadNorth <iksadNorth@gmail.com>
2023-10-05 00:13:06 +0900

    init project

---
+++ src/main/java/com/iksad/chattinglab/ChattingLabApplication.java
@@ -0,0 +1,13 @@
+package com.iksad.chattinglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iksad/chattinglab/ChattingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iksad.chattinglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
